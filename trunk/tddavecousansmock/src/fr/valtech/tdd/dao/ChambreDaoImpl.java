ca067f9aa66ab6f897a0f3b9cd8b9b59f16a2e08
anthony.dahanne <anthony.dahanne@e1b8a4b8-42e0-11de-b6cc-f32266f7ba4a>
2009-05-17 21:40:03 +0000

    premier business ConsulterDiponibiliteChambre

--- trunk/tddavecousansmock/src/fr/valtech/tdd/dao/ChambreDaoImpl.java
+++ trunk/tddavecousansmock/src/fr/valtech/tdd/dao/ChambreDaoImpl.java
@@ -1,5 +1,7 @@
 package fr.valtech.tdd.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -32,4 +34,18 @@ public class ChambreDaoImpl implements ChambreDao {
 		return chambre;
 	}
 
+	@Override
+	public List<Chambre> findByCapacite(int capacite) {
+		EntityManager entityManager = factory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		Query q = entityManager
+				.createQuery("select c from Chambre c where c.capacite = :capacite");
+		q.setParameter("capacite", capacite);
+		List<Chambre> chambres = q.getResultList();
+
+		entityManager.getTransaction().rollback();
+		entityManager.close();
+		return chambres;
+	}
 }
